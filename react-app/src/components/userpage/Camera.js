41ebe6298fdd1f46309c1065cd0ddb42947429fb
Derek <derekkiminho@gmail.com>
2021-01-07 11:10:20 -0800

    Added webcam component so users can take a photo to upload for their profile picture

---
+++ react-app/src/components/userpage/Camera.js
@@ -0,0 +1,179 @@
+import React, {useState, useRef, useCallback, useEffect} from "react";
+import { Redirect, useHistory } from 'react-router-dom';
+import Webcam from "react-webcam";
+import {setPicture} from '../../services/user'
+import {setUser} from '../../redux/actions/users';
+import {useSelector, useDispatch} from 'react-redux'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import {uploadImage, getPosts} from '../../services/post'
+
+const videoConstraints = {
+    width: 600,
+    height: 600,
+    facingMode: "user"
+};
+
+const WebcamComponent = () => {
+    const currentUser = useSelector(state => state.users.user)
+    const webcamRef = useRef(null)
+    const [imgSrc, setImgSrc] = useState(null)
+    const [image, setImage] = useState({})
+    const [fileName, setFileName] = useState("")
+    const [caption, setCaption] = useState("")
+    const [redirect, setRedirect] = useState(false)
+    const [posts, setPosts] = useState(0)
+    const [preview, setPreview] = useState(true)
+    const dispatch = useDispatch()
+    const history = useHistory()
+
+    useEffect(() => {
+        (async () => {
+            const allPosts = await getPosts(currentUser.id)
+            setPosts(allPosts.posts.length)
+        })()
+    },[currentUser.id])
+
+    useEffect(() => {
+        (async () => {
+            if(imgSrc && posts){
+                setFileName(currentUser.fullname + posts.toString())
+            }
+
+        })()
+    },[imgSrc])
+
+    useEffect(() => {
+        if(fileName){
+            setImage(dataURLtoFile(imgSrc, fileName))
+        }
+    }, [fileName, imgSrc])
+
+    // This file takes the base64 encoded data from the webcam, and converts it to a file to send to S3.
+    const dataURLtoFile = (dataurl, filename) => {
+
+        var arr = dataurl.split(','),
+            mime = arr[0].match(/:(.*?);/)[1],
+            bstr = atob(arr[1]),
+            n = bstr.length,
+            u8arr = new Uint8Array(n);
+
+        while(n--){
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+
+        return new File([u8arr], filename, {type:mime});
+    }
+
+    const updateValue = (setfunc) => (e) => {
+        setfunc(e.target.value)
+    }
+
+    const setPreviewHelper = () => {
+        if (preview){
+            setPreview(false)
+
+        } else {
+            setPreview(true)
+        }
+    }
+
+    const capture = useCallback(
+        () => {
+            setImgSrc(webcamRef.current.getScreenshot())
+
+        },[webcamRef])
+
+    // const submitImagePost = async (e) => {
+    //     e.preventDefault();
+    //     const data = new FormData();
+    //     data.append("file", image)
+    //     console.log(image)
+
+    //     const newImage = await uploadImage(data)
+    //     const imageUrl = newImage.output
+
+    //     const post = await createPost( caption, imageUrl);
+    //     if(!post.errors){
+    //         setRedirect(true)
+
+    //     } else {
+    //         console.log('something went wrong', image)
+    //     }
+    // }
+
+    const submitProfilePicture = async (e) => {
+        e.preventDefault();
+        const data = new FormData();
+
+        data.append("file", image)
+
+        const newImage = await uploadImage(data)
+        const imageUrl = newImage.output
+
+        const user = await setPicture(imageUrl)
+        if(user){
+            dispatch(setUser(user))
+            history.push(`/user/${currentUser.id}`)
+            window.location.reload()
+        }
+
+    }
+
+    if(redirect){
+        return <Redirect to={`/`} />
+    }
+
+
+    return (
+        <div className="webcam_container">
+            <div className="webcam_center">
+                <div className="webcam_camera_container">
+                    <Webcam
+                    audio={false}
+                    height={600}
+                    width={600}
+                    ref={webcamRef}
+                    screenshotFormat="image/jpeg"
+                    videoConstraints={videoConstraints}
+                    />
+                </div>
+                <div className="webcam_capture_button">
+                    <Button className="edit_button" color="primary" variant="contained" onClick={capture}>Capture Photo</Button>
+                    {preview ? <Button className="edit_button" color="primary" variant="outlined" onClick={setPreviewHelper}>Hide Preview</Button> :
+                        <Button className="edit_button" color="primary" variant="contained" onClick={setPreviewHelper}>Show Preview</Button>}
+                </div>
+            </div>
+            <div className="webcam_caption">
+                <form className="webcam_form" encType='multipart/formdata' onSubmit={submitProfilePicture}>
+                    {/* <TextField
+                            name='caption'
+                            type='text'
+                            label="Caption"
+                            variant="outlined"
+                            placeholder='Tell us about your image...'
+                            value={caption}
+                            onChange={updateValue(setCaption)}
+                            required
+                            fullWidth
+                            /> */}
+                    {/* <TextField fullWidth variant="outlined" type="file" name="user_file" required onChange={setImageHelper}/> */}
+                    <Button className="edit_button" color="primary" variant="contained" type="submit">Post</Button>
+                </form>
+            </div>
+            <div className="camera_image_preview">
+                {preview && imgSrc &&
+                (<div className="image_preview">
+                    <div className="image_preview_header">Preview Photo</div>
+                    <img
+                    src={imgSrc}
+                    height={400}
+                    width={400}
+                    />
+                </div>)}
+            </div>
+        </div>
+    )
+}
+
+export default WebcamComponent
--- react-app/src/components/userpage/ProfilePicture.js
+++ react-app/src/components/userpage/ProfilePicture.js
@@ -5,17 +5,27 @@ import {uploadImage} from '../../services/post'
 import {setPicture} from '../../services/user'
 import {setUser} from '../../redux/actions/users';
 import TextField from '@material-ui/core/TextField';
+import WebcamComponent from './Camera'
 import Button from '@material-ui/core/Button'
 import NavBar from '../NavBar'
 
 const ProfilePictureUpload = ({setAuthenticated}) => {
     const currentUser = useSelector(state => state.users.user)
     const [image, setImage] = useState({})
+    const [postType, setPostType] = useState("upload")
     const dispatch = useDispatch()
     let history = useHistory()
 
     const setImageHelper = (e) => {
         setImage(e.target.files[0])
+    }
+
+    const setUploadPostType = () => {
+        setPostType("upload")
+    }
+
+    const setCameraPostType = () => {
+        setPostType("camera")
     }
 
     const submitProfilePicture = async (e) => {
@@ -36,6 +46,27 @@ const ProfilePictureUpload = ({setAuthenticated}) => {
 
     }
 
+    if(postType === "camera"){
+        return (
+            <div className="top_userpage_container">
+            <NavBar setAuthenticated={setAuthenticated}/>
+            <div className="createpost_container">
+                <div className="createpost_webcam_container_center">
+                    <div className="createpost_header">
+                    Update Profile Picture
+                    </div>
+                    <div className="createpost_type_button">
+                        <Button onClick={setUploadPostType} variant="contained" color="primary">Upload Image</Button>
+                        <Button onClick={setCameraPostType} variant="contained" color="primary">Take Photo</Button>
+                    </div>
+                    <WebcamComponent />
+                </div>
+            </div>
+
+        </div>
+        )
+    }
+
     return (
         <div className="top_userpage_container">
             <NavBar setAuthenticated={setAuthenticated}/>
@@ -46,6 +77,10 @@ const ProfilePictureUpload = ({setAuthenticated}) => {
                     </div>
                     <div className="update_profile_picture_title">
                         choose a photo to upload
+                    </div>
+                    <div className="createpost_type_button">
+                        <Button onClick={setUploadPostType} variant="contained" color="primary">Upload Image</Button>
+                        <Button onClick={setCameraPostType} variant="contained" color="primary">Take Photo</Button>
                     </div>
                     <form className="update_profile_picture_form" encType='multipart/formdata' onSubmit={submitProfilePicture}>
                         <TextField fullWidth variant="outlined" type="file" name="user_file" required onChange={setImageHelper}/>
