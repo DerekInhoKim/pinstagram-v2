37c9b82c892919760a08751483611cf58e992257
Derek <derekkiminho@gmail.com>
2021-03-17 15:15:27 -0700

    Added clarifying comments for a users request

--- react-app/src/services/user.js
+++ react-app/src/services/user.js
@@ -1,14 +1,17 @@
+// Returns a user's info
 export const getUser = async(userId) => {
     const response = await fetch(`/api/users/${userId}`)
     return await response.json()
 
 }
 
+// Returns a user's followers
 export const getUserFollowers = async(userId) => {
     const response = await (fetch(`/api/users/follow/${userId}`))
     return await response.json()
 }
 
+// Updates the user info's profile picture
 export const setPicture = async(imageUrl) => {
     const response = await fetch('/api/users/profilePicture', {
         method:"PUT",
