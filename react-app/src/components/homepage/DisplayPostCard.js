36c0a9d78bde9489d1fcb47b0613a7ade0ccb179
Derek <derekkiminho@gmail.com>
2021-04-21 11:05:35 -0700

    cleaning up code for workshop

--- react-app/src/components/homepage/DisplayPostCard.js
+++ react-app/src/components/homepage/DisplayPostCard.js
@@ -14,30 +14,30 @@ import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 
 
 const DisplayPost = ({id, caption, content, createdAt, user}) => {
-    const currentUser = useSelector(state => state.users.user)
-    const currentComments = useSelector(state => state.comments)
+    const currentUser = useSelector(state => state.users.user);
+    const currentComments = useSelector(state => state.comments);
 
-    const [likes, setLikes] = useState([])
-    const [userLike, setUserLike] = useState(false)
-    const [comments, setComments] = useState([])
+    const [likes, setLikes] = useState([]);
+    const [userLike, setUserLike] = useState(false);
+    const [comments, setComments] = useState([]);
 
     // This use effect sends a request to see if a user is already liking a post.
     // If a user already likes a post, it displays the liked post version of the post.
     useEffect(() => {
         (async () => {
-            const likesResponse = await userLikesPost(id, currentUser.id)
-            setUserLike(likesResponse.likes)
+            const likesResponse = await userLikesPost(id, currentUser.id);
+            setUserLike(likesResponse.likes);
         })()
-    }, [id])
+    }, [id]);
 
 
     // This use effect sends a request to get the number of likes for a post
     useEffect(() => {
         (async () => {
-            const likesResponse = await getPostLikes(id)
-            setLikes(likesResponse.like)
+            const likesResponse = await getPostLikes(id);
+            setLikes(likesResponse.like);
         })()
-    }, [userLike, id])
+    }, [userLike, id]);
 
     // This use effects fires off a request to grab all comments for a post
     // Pass in the currentComments in the dependency array
@@ -46,29 +46,31 @@ const DisplayPost = ({id, caption, content, createdAt, user}) => {
     // Deleting a comment will need to be flushed out more thoroughly
     useEffect(() => {
         (async () => {
-            const commentsResponse = await getComments(id)
-            setComments(commentsResponse.comments)
+            const commentsResponse = await getComments(id);
+            setComments(commentsResponse.comments);
         })()
-    }, [currentComments])
+    }, [currentComments]);
 
 
     // Handles the button click when a user likes a post
     const handleLike = async () => {
-        const response = await likePost(id, currentUser.id)
+        const response = await likePost(id, currentUser.id);
         if (response.error) {
-            alert(response.error)
+            alert(response.error);
         }
-        setUserLike(true)
+        setUserLike(true);
     }
 
     // Handles the button click when a user dislikes/unlikes a post
     const handleDislike = async () => {
-        const response = await dislikePost(id, currentUser.id)
-        setUserLike(false)
+        const response = await dislikePost(id, currentUser.id);
+        if(response.error){
+            alert(response.error);
+        }
+        setUserLike(false);
     }
 
-    // Used to display each comment
-
+    // Used to display each comment, but only the last 3
     const commentComponent = comments.slice(0,3).map(comment => {
         return (
             <DisplayHomePageComments key={comment.id} comment={comment}/>
