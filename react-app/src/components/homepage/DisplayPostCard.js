4724276613e808a0c5397dc543e295bfc781f1b1
Derek <derekkiminho@gmail.com>
2021-04-21 11:03:30 -0700

    cleaning up code as part of workshop

--- react-app/src/components/homepage/DisplayPostCard.js
+++ react-app/src/components/homepage/DisplayPostCard.js
@@ -1,17 +1,16 @@
-import React, {useEffect, useState} from 'react'
-import {useSelector} from 'react-redux'
-import {Link} from 'react-router-dom'
-import PostHeader from './PostHeader'
-import DisplayHomePageComments from '../comments/DisplayHomePageComments'
-import CommentForm from '../comments/CommentForm'
-import {getPostLikes, likePost, userLikesPost, dislikePost} from '../../services/likes'
+import React, {useEffect, useState} from 'react';
+import {useSelector} from 'react-redux';
+import {Link} from 'react-router-dom';
+import PostHeader from './PostHeader';
+import DisplayHomePageComments from '../comments/DisplayHomePageComments';
+import CommentForm from '../comments/CommentForm';
+import {getPostLikes, likePost, userLikesPost, dislikePost} from '../../services/likes';
 import {getComments} from '../../services/comments'
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import IconButton from '@material-ui/core/IconButton';
 import FiberPinIcon from '@material-ui/icons/FiberPin';
-import {makeStyles} from '@material-ui/core'
-import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 
 
 const DisplayPost = ({id, caption, content, createdAt, user}) => {
